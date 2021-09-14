f98db7f37cf55f3707fc3f338e60b669b7cee14c
Teerapong225 <s6135512041@phuket.psu.ac.th>
2021-09-14 23:45:47 +0700

    message

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard'; 
+import WordCard from './WordCard';
+
+let word = "Hello" 
+function App() {
+  return (
+    <div>       
+      
+        <WordCard value="HELLO"/>  
+      
+      </div>
+  );
+}
+
+export default App;
